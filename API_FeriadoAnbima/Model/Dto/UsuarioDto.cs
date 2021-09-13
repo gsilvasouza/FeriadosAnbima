e6e87f36d5c5696f3f62c4afedf5b55d4b3810bb
gsilvasouza <gsilvasouza2000@gmail.com>
2021-09-13 17:03:51 -0300

    Criação da classe UsuarioDto

---
+++ API_FeriadoAnbima/Model/Dto/UsuarioDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_FeriadoAnbima.Model.Dto
+{
+    public class UsuarioDto
+    {
+        public int ID { get; set; }
+        public string nome { get; set; }
+        public string senha { get; set; }
+        public string email { get; set; }
+
+        public UsuarioDto()
+        {
+        }
+
+        public UsuarioDto(int iD, string nome, string senha, string email)
+        {
+            ID = iD;
+            this.nome = nome;
+            this.senha = senha;
+            this.email = email;
+        }
+    }
+}
