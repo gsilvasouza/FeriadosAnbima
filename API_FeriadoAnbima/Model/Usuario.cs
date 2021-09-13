5f1ec564a11eaa5269be0d6e37fac46c9c43f433
gsilvasouza <gsilvasouza2000@gmail.com>
2021-09-13 17:03:34 -0300

    Criação da entidade Usuario

---
+++ API_FeriadoAnbima/Model/Usuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_FeriadoAnbima.Model
+{
+    [Table("TB_Usuario")]
+    public class Usuario
+    {
+        [Key]
+        [Column("ID")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Column("Nome")]
+        [Required]
+        public string nome { get; set; }
+
+        [Column("Senha")]
+        [Required]
+        public string senha { get; set; }
+
+        [Column("Email")]
+        [Required]
+        public string email { get; set; }
+
+        public Usuario()
+        {
+        }
+    }
+}
